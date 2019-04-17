440f426f50ef4d4e893a42cf41454ba879a851e5
Alexandr Krasnij <php4.drvn@gmail.com>
2019-04-17 17:36:19 +0300

    Added account API

---
+++ catalog/controller/extension/d_vuefront/account.php
@@ -0,0 +1,75 @@
+<?php
+
+class ControllerExtensionDVuefrontAccount extends Controller
+{
+    private $codename = "d_vuefront";
+    private $sub_versions = array('lite', 'light', 'free');
+    private $config_file = '';
+    private $setting = array();
+
+    public function register($args)
+    {
+        $this->load->model('account/customer');
+        $customer_info = $args['customer'];
+        $customerData = array(
+            'firstname' => $customer_info['firstName'],
+            'lastname' => $customer_info['lastName'],
+            'email' => $customer_info['email'],
+            'telephone' => '',
+            'password' => $customer_info['password'],
+        );
+
+        $customer_id = $this->model_account_customer->addCustomer($customerData);
+
+        $customer_info = $this->model_account_customer->getCustomer($customer_id);
+
+        $this->customer->login($customer_info['email'], $customer_info['password']);
+
+        unset($this->session->data['guest']);
+
+        return array(
+            'id' => $customer_info['customer_id'],
+            'firstName' => $customer_info['firstname'],
+            'lastName' => $customer_info['lastname'],
+            'email' => $customer_info['email'],
+        );
+    }
+
+    public function login($args)
+    {
+        $this->load->model('account/customer');
+        if ($this->customer->login($args['email'], $args['password'])) {
+            $customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
+
+            return array(
+                'id' => $customer_info['customer_id'],
+                'firstName' => $customer_info['firstname'],
+                'lastName' => $customer_info['lastname'],
+                'email' => $customer_info['email'],
+            );
+        }
+    }
+
+    public function isLogged()
+    {
+        $this->load->model('account/customer');
+        $customer_info = array();
+        $customer = array();
+        if ($this->customer->isLogged()) {
+            $customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
+            $customer = array(
+                'id' => $customer_info['customer_id'],
+                'firstName' => $customer_info['firstname'],
+                'lastName' => $customer_info['lastname'],
+                'email' => $customer_info['email'],
+            );
+        }
+        
+        $logged = $this->customer->isLogged();
+
+        return array(
+            'status' => !empty($logged),
+            'customer' => $customer
+        );
+    }
+}
---
+++ catalog/controller/extension/d_vuefront_type/account.php
@@ -0,0 +1,112 @@
+<?php
+
+use Youshido\GraphQL\Type\Object\ObjectType;
+use Youshido\GraphQL\Type\InputObject\InputObjectType;
+use Youshido\GraphQL\Type\Scalar\StringType;
+use Youshido\GraphQL\Type\Scalar\IntType;
+use Youshido\GraphQL\Type\ListType\ListType;
+use Youshido\GraphQL\Type\Scalar\BooleanType;
+
+class ControllerExtensionDVuefrontTypeAccount extends Controller
+{
+    private $codename = "d_vuefront";
+
+    public function query()
+    {
+        return array();
+    }
+
+    public function mutation()
+    {
+        return array(
+            'accountLogin'  => array(
+                'type'    => $this->customerType(),
+                'args'    => array(
+                    'email'       => array(
+                        'type' => new StringType(),
+                    ),
+                    'password' => array(
+                        'type'         => new StringType()
+                    )
+                ),
+                'resolve' => function ($store, $args) {
+                    return $this->load->controller('extension/'.$this->codename.'/account/login', $args);
+                }
+            ),
+            'accountRegister' => array(
+                'type'    => $this->customerType(),
+                'args'    => array(
+                    'customer'      => array(
+                        'type' =>$this->customerInputType()
+                    )
+                ),
+                'resolve' => function ($store, $args) {
+                    return $this->load->controller('extension/'.$this->codename.'/account/register', $args);
+                }
+            ),
+            'accountCheckLogged' => array(
+                'type' => new ObjectType(
+                    array(
+                        'name' => 'LoggedResult',
+                        'description' => 'LoggedResult',
+                        'fields' => array(
+                            'status' => new BooleanType(),
+                            'customer' => $this->customerType()
+                        )
+                    )
+                ),
+                'resolve' => function ($parent, $args) {
+                    return $this->load->controller('extension/'.$this->codename.'/account/isLogged', $args);
+                }
+            )
+        );
+    }
+
+    public function customerType()
+    {
+        return new ObjectType(
+            array(
+                'name'        => 'Customer',
+                'description' => 'Customer',
+                'fields'      => array(
+                    'id'    => new StringType(),
+                    'firstName' => new StringType(),
+                    'lastName' => new StringType(),
+                    'email' => new StringType()
+                )
+            )
+        );
+    }
+
+    private function customerInputType()
+    {
+        return new InputObjectType(
+            array(
+                'name'        => 'CustomerInput',
+                'description' => 'CustomerInput',
+                'fields'      => array(
+                    'firstName' => new StringType(),
+                    'lastName' => new StringType(),
+                    'email' => new StringType(),
+                    'password' => new StringType()
+                )
+            )
+        );
+    }
+
+    public function cartProductType()
+    {
+        return new ObjectType(
+            array(
+                'name'        => 'CartProduct',
+                'description' => 'CartProduct',
+                'fields'      => array(
+                    'key'      => new StringType(),
+                    'product'  => $this->load->controller('extension/'.$this->codename.'_type/product/productType'),
+                    'quantity' => new IntType(),
+                    'total'    => new StringType()
+                )
+            )
+        );
+    }
+}
