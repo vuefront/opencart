129365fb83a4fa6a9f1c8f3a58e1bc95a2087b4e
Alexandr Krasnij <php4.drvn@gmail.com>
2019-04-30 10:43:22 +0300

    Added cors support

--- catalog/controller/extension/module/d_vuefront.php
+++ catalog/controller/extension/module/d_vuefront.php
@@ -4,7 +4,6 @@ require_once DIR_SYSTEM . 'library/d_graphql/vendor/autoload.php';
 use GraphQL\GraphQL;
 use GraphQL\Utils\BuildSchema;
 
-
 class ControllerExtensionModuleDVuefront extends Controller
 {
     private $codename = "d_vuefront";
@@ -14,6 +13,7 @@ class ControllerExtensionModuleDVuefront extends Controller
     {
         $this->load->model($this->route);
         $resolvers = $this->model_extension_module_d_vuefront->getResolvers();
+
         try {
             $schema = BuildSchema::build(file_get_contents(__DIR__ . '/'.$this->codename.'/schema.graphql'));
             $rawInput = file_get_contents('php://input');
@@ -28,6 +28,14 @@ class ControllerExtensionModuleDVuefront extends Controller
                 ]
             ];
         }
+
+        if (!empty($this->request->get['cors'])) {
+            $this->response->addHeader('Access-Control-Allow-Origin: '.$this->request->server['HTTP_ORIGIN']);
+            $this->response->addHeader('Access-Control-Allow-Methods: POST, OPTIONS');
+            $this->response->addHeader('Access-Control-Allow-Credentials: true');
+            $this->response->addHeader('Access-Control-Allow-Headers: DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Token,token,Cookie,cookie');
+        }
+
         $this->response->addHeader('Content-Type: application/json; charset=UTF-8');
         $this->response->setOutput(json_encode($result));
     }
