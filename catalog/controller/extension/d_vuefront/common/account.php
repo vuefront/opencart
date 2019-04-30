ecb7019eb04bcae7d85e5119aec76438f839e368
Alexandr Krasnij <php4.drvn@gmail.com>
2019-04-30 16:10:10 +0300

    Added api for address

--- catalog/controller/extension/d_vuefront/common/account.php
+++ catalog/controller/extension/d_vuefront/common/account.php
@@ -1,126 +1,175 @@
 <?php
 
-class ControllerExtensionDVuefrontCommonAccount extends Controller
-{
-    public function register($args)
-    {
-        $this->load->model('account/customer');
-        $this->load->language('account/register');
-        $customer_info = $args['customer'];
+class ControllerExtensionDVuefrontCommonAccount extends Controller {
+	public function register( $args ) {
+		$this->load->model( 'account/customer' );
+		$this->load->language( 'account/register' );
+		$customer_info = $args['customer'];
 
-        if ($this->model_account_customer->getTotalCustomersByEmail($customer_info['email'])) {
-            throw new Exception($this->language->get('error_exists'));
+		if ( $this->model_account_customer->getTotalCustomersByEmail( $customer_info['email'] ) ) {
+			throw new Exception( $this->language->get( 'error_exists' ) );
 		}
-        $customerData = array(
-            'firstname' => $customer_info['firstName'],
-            'lastname' => $customer_info['lastName'],
-            'email' => $customer_info['email'],
-            'telephone' => '',
-            'password' => $customer_info['password'],
-        );
+		$customerData = array(
+			'firstname' => $customer_info['firstName'],
+			'lastname'  => $customer_info['lastName'],
+			'email'     => $customer_info['email'],
+			'telephone' => '',
+			'password'  => $customer_info['password'],
+		);
 
-        $customer_id = $this->model_account_customer->addCustomer($customerData);
+		$customer_id = $this->model_account_customer->addCustomer( $customerData );
 
-        $customer_info = $this->model_account_customer->getCustomer($customer_id);
+		$customer_info = $this->model_account_customer->getCustomer( $customer_id );
 
-        $this->customer->login($customer_info['email'], $customer_info['password']);
+		$this->customer->login( $customer_info['email'], $customer_info['password'] );
 
-        unset($this->session->data['guest']);
+		unset( $this->session->data['guest'] );
 
-        return array(
-            'id' => $customer_info['customer_id'],
-            'firstName' => $customer_info['firstname'],
-            'lastName' => $customer_info['lastname'],
-            'email' => $customer_info['email'],
-        );
-    }
+		return array(
+			'id'        => $customer_info['customer_id'],
+			'firstName' => $customer_info['firstname'],
+			'lastName'  => $customer_info['lastname'],
+			'email'     => $customer_info['email'],
+		);
+	}
 
-    public function login($args)
-    {
-        $this->load->language('account/login');
-        $this->load->model('account/customer');
-        if ($this->customer->login($args['email'], $args['password'])) {
-            $customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
+	public function login( $args ) {
+		$this->load->language( 'account/login' );
+		$this->load->model( 'account/customer' );
+		if ( $this->customer->login( $args['email'], $args['password'] ) ) {
+			$customer_info = $this->model_account_customer->getCustomer( $this->customer->getId() );
 
-            return array(
-                'id' => $customer_info['customer_id'],
-                'firstName' => $customer_info['firstname'],
-                'lastName' => $customer_info['lastname'],
-                'email' => $customer_info['email'],
-            );
-        } else {
-            throw new Exception($this->language->get('error_login'));
-        }
-    }
+			return array(
+				'id'        => $customer_info['customer_id'],
+				'firstName' => $customer_info['firstname'],
+				'lastName'  => $customer_info['lastname'],
+				'email'     => $customer_info['email'],
+			);
+		} else {
+			throw new Exception( $this->language->get( 'error_login' ) );
+		}
+	}
 
-    public function logout()
-    {
-        $this->customer->logout();
+	public function logout() {
+		$this->customer->logout();
 
-        $logged = $this->customer->isLogged();
+		$logged = $this->customer->isLogged();
 
-        return array(
-            'status' => !empty($logged)
-        );
-    }
+		return array(
+			'status' => ! empty( $logged )
+		);
+	}
 
-    public function edit($args) {
-	    $this->load->model('account/customer');
-	    $customer_info = $args['customer'];
-	    $customerData = array(
-		    'firstname' => $customer_info['firstName'],
-		    'lastname' => $customer_info['lastName'],
-		    'email' => $customer_info['email'],
-		    'telephone' => ''
-	    );
+	public function edit( $args ) {
+		$this->load->model( 'account/customer' );
+		$customer_info = $args['customer'];
+		$customerData  = array(
+			'firstname' => $customer_info['firstName'],
+			'lastname'  => $customer_info['lastName'],
+			'email'     => $customer_info['email'],
+			'telephone' => ''
+		);
 
-	    $this->model_account_customer->editCustomer($this->customer->getId(), $customerData);
+		$this->model_account_customer->editCustomer( $this->customer->getId(), $customerData );
 
-	    $customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
+		$customer_info = $this->model_account_customer->getCustomer( $this->customer->getId() );
 
-	    return array(
-		    'id' => $customer_info['customer_id'],
-		    'firstName' => $customer_info['firstname'],
-		    'lastName' => $customer_info['lastname'],
-		    'email' => $customer_info['email'],
-	    );
-    }
+		return array(
+			'id'        => $customer_info['customer_id'],
+			'firstName' => $customer_info['firstname'],
+			'lastName'  => $customer_info['lastname'],
+			'email'     => $customer_info['email'],
+		);
+	}
 
-    public function editPassword($args) {
-	    $this->load->model('account/customer');
+	public function editPassword( $args ) {
+		$this->load->model( 'account/customer' );
 
-        $this->model_account_customer->editPassword($this->customer->getEmail(), $args['password']);
+		$this->model_account_customer->editPassword( $this->customer->getEmail(), $args['password'] );
 
-	    $customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
+		$customer_info = $this->model_account_customer->getCustomer( $this->customer->getId() );
 
-	    return array(
-		    'id' => $customer_info['customer_id'],
-		    'firstName' => $customer_info['firstname'],
-		    'lastName' => $customer_info['lastname'],
-		    'email' => $customer_info['email'],
-	    );
-    }
+		return array(
+			'id'        => $customer_info['customer_id'],
+			'firstName' => $customer_info['firstname'],
+			'lastName'  => $customer_info['lastname'],
+			'email'     => $customer_info['email'],
+		);
+	}
 
-    public function isLogged()
-    {
-        $this->load->model('account/customer');
-        $customer_info = array();
-        $customer = array();
-        if ($this->customer->isLogged()) {
-            $customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
-            $customer = array(
-                'id' => $customer_info['customer_id'],
-                'firstName' => $customer_info['firstname'],
-                'lastName' => $customer_info['lastname'],
-                'email' => $customer_info['email'],
-            );
-        }
-        
-        $logged = $this->customer->isLogged();
+	public function isLogged() {
+		$this->load->model( 'account/customer' );
+		$customer_info = array();
+		$customer      = array();
+		if ( $this->customer->isLogged() ) {
+			$customer_info = $this->model_account_customer->getCustomer( $this->customer->getId() );
+			$customer      = array(
+				'id'        => $customer_info['customer_id'],
+				'firstName' => $customer_info['firstname'],
+				'lastName'  => $customer_info['lastname'],
+				'email'     => $customer_info['email'],
+			);
+		}
 
-        return array(
-            'status' => !empty($logged),
-            'customer' => $customer
-        );
-    }
+		$logged = $this->customer->isLogged();
+
+		return array(
+			'status'   => ! empty( $logged ),
+			'customer' => $customer
+		);
+	}
+
+	public function addressList() {
+		$this->load->model( 'account/address' );
+
+		$results   = $this->model_account_address->getAddresses();
+		$addresses = array();
+		foreach ( $results as $result ) {
+			$addresses[] = array(
+				'id'        => $result['address_id'],
+				'firstName' => $result['firstname'],
+				'lastName'  => $result['lastname'],
+				'company'   => $result['company'],
+				'address1'  => $result['address_1'],
+				'address2'  => $result['address_2'],
+				'city'      => $result['city'],
+				'zipcode'   => $result['postcode'],
+			);
+		}
+
+		return $addresses;
+	}
+
+	public function address( $args ) {
+		$this->load->model( 'account/address' );
+
+		$result = $this->model_account_address->getAddress( $args['id'] );
+
+		return array(
+			'id'        => $result['address_id'],
+			'firstName' => $result['firstname'],
+			'lastName'  => $result['lastname'],
+			'company'   => $result['company'],
+			'address1'  => $result['address_1'],
+			'address2'  => $result['address_2'],
+			'city'      => $result['city'],
+			'zipcode'   => $result['postcode'],
+		);
+	}
+
+	public function addAddress($args) {
+		$this->load->model( 'account/address' );
+
+		$address_id = $this->model_account_address->addAddress($this->customer->getId(), $args['address']);
+
+		return $this->address(array('id' => $address_id));
+	}
+
+	public function editAddress($args) {
+		$this->load->model( 'account/address' );
+
+		$this->model_account_address->editAddress($args['id'], $args['address']);
+
+		return $this->address(array('id' => $args['id']));
+	}
 }
