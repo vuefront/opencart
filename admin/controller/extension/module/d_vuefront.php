caa318a315fcfb87717b76a01ce3843982dce063
dmitriyzhuk <dmitriyzhuk@gmail.com>
2019-06-03 17:31:59 +0300

    removed blog as a dependency

--- admin/controller/extension/module/d_vuefront.php
+++ admin/controller/extension/module/d_vuefront.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
-* location: admin/controller
-*/
+ * location: admin/controller
+ */
 class ControllerExtensionModuleDVuefront extends Controller
 {
     private $codename = 'd_vuefront';
@@ -17,9 +17,10 @@ class ControllerExtensionModuleDVuefront extends Controller
         $this->load->model('extension/d_opencart_patch/url');
         $this->load->model('extension/d_opencart_patch/load');
 
-        $this->d_shopunity = (file_exists(DIR_SYSTEM.'library/d_shopunity/extension/d_shopunity.json'));
-        $this->extension = json_decode(file_get_contents(DIR_SYSTEM.'library/d_shopunity/extension/d_vuefront.json'), true);
-        $this->d_admin_style = (file_exists(DIR_SYSTEM.'library/d_shopunity/extension/d_admin_style.json'));
+        $this->d_shopunity = (file_exists(DIR_SYSTEM . 'library/d_shopunity/extension/d_shopunity.json'));
+        $this->d_blog_module = (file_exists(DIR_SYSTEM . 'library/d_shopunity/extension/d_blog_module.json'));
+        $this->extension = json_decode(file_get_contents(DIR_SYSTEM . 'library/d_shopunity/extension/d_vuefront.json'), true);
+        $this->d_admin_style = (file_exists(DIR_SYSTEM . 'library/d_shopunity/extension/d_admin_style.json'));
     }
 
     public function index()
@@ -39,6 +40,8 @@ class ControllerExtensionModuleDVuefront extends Controller
             $this->model_extension_d_admin_style_style->getStyles('light');
         }
 
+        $this->document->addScript('view/javascript/d_vuefront/clipboard/clipboard.min.js');
+
         $url_params = array();
         $url = '';
 
@@ -46,10 +49,17 @@ class ControllerExtensionModuleDVuefront extends Controller
 
         $this->document->setTitle($this->language->get('heading_title_main'));
         $data['heading_title'] = $this->language->get('heading_title_main');
+        $data['d_blog_module'] = $this->d_blog_module;
 
         $data['text_edit'] = $this->language->get('text_edit');
+        $data['text_copy'] = $this->language->get('text_copy');
         $data['text_title'] = $this->language->get('text_title');
         $data['text_description'] = $this->language->get('text_description');
+
+        $data['text_blog_module'] = $this->language->get('text_blog_module');
+        $data['text_blog_enabled'] = $this->language->get('text_blog_enabled');
+        $data['text_blog_disabled'] = $this->language->get('text_blog_disabled');
+        $data['text_blog_description'] = $this->language->get('text_blog_description');
 
         // Button
         $data['button_cancel'] = $this->language->get('button_cancel');
@@ -71,23 +81,23 @@ class ControllerExtensionModuleDVuefront extends Controller
 
         $data['breadcrumbs'][] = array(
             'text' => $this->language->get('text_home'),
-            'href' => $this->model_extension_d_opencart_patch_url->link('common/dashboard')
-            );
-    
+            'href' => $this->model_extension_d_opencart_patch_url->link('common/dashboard'),
+        );
+
         $data['breadcrumbs'][] = array(
             'text' => $this->language->get('text_module'),
-            'href' => $this->model_extension_d_opencart_patch_url->getExtensionLink('module')
-            );
+            'href' => $this->model_extension_d_opencart_patch_url->getExtensionLink('module'),
+        );
 
         $data['breadcrumbs'][] = array(
             'text' => $this->language->get('heading_title_main'),
-            'href' => $this->model_extension_d_opencart_patch_url->link($this->route, $url)
-            );
-            
+            'href' => $this->model_extension_d_opencart_patch_url->link($this->route, $url),
+        );
+
         if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
-            $data['catalog'] = HTTPS_CATALOG.'index.php?route=extension/module/d_vuefront/graphql';
+            $data['catalog'] = HTTPS_CATALOG . 'index.php?route=extension/module/d_vuefront/graphql';
         } else {
-            $data['catalog'] = HTTP_CATALOG.'index.php?route=extension/module/d_vuefront/graphql';
+            $data['catalog'] = HTTP_CATALOG . 'index.php?route=extension/module/d_vuefront/graphql';
         }
 
         //action
--- admin/language/en-gb/extension/module/d_vuefront.php
+++ admin/language/en-gb/extension/module/d_vuefront.php
@@ -4,17 +4,20 @@
  */
 
 //heading
-$_['heading_title']             = '<span style="color:#449DD0; font-weight:bold">Vuefront</span><span style="font-size:12px; color:#999"> by <a href="https://www.opencart.com/index.php?route=marketplace/extension&filter_member=Dreamvention" style="font-size:1em; color:#999" target="_blank">Dreamvention</a></span>';
-$_['heading_title_main']        = 'Vuefront';
-$_['text_edit']                 = 'Edit Vuefront settings';
-$_['text_module']               = 'Modules';
+$_['heading_title'] = '<span style="color:#449DD0; font-weight:bold">Vuefront</span><span style="font-size:12px; color:#999"> by <a href="https://www.opencart.com/index.php?route=marketplace/extension&filter_member=Dreamvention" style="font-size:1em; color:#999" target="_blank">Dreamvention</a></span>';
+$_['heading_title_main'] = 'Vuefront';
+$_['text_edit'] = 'Edit Vuefront settings';
+$_['text_module'] = 'Modules';
+$_['text_copy'] = 'copy';
 
-$_['text_title']                = 'CMS Connect URL';
-$_['text_description']          = 'This is your CMS Connect URL link that shares your Store data via GraphQL. When installing VueFront via the command line, you will be prompted to enter this URL. Simply copy and paste it into the command line. 
-<br>
+$_['text_title'] = 'CMS Connect URL';
+$_['text_description'] = 'This is your CMS Connect URL link that shares your Store data via GraphQL. When installing VueFront via the command line, you will be prompted to enter this URL. Simply copy and paste it into the command line.
+<br><br>
 Read more about the <a href="https://vuefront.com/cms/opencart.html" target="_blank">CMS Connect for OpenCart</a>';
 
+$_['text_blog_module'] = 'Blog support';
+$_['text_blog_enabled'] = 'Blog enabled';
+$_['text_blog_disabled'] = 'Blog module missing. Click to download';
+$_['text_blog_description'] = 'VueFront relies on the free <a href="https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=10468" target="_blank">Blog Module</a> by Dreamvention to implement blog support. You can install it via OpenCart Marketplace or Shopunity.';
 //support
-$_['text_powered_by']			= 'Tested with <a href="https://shopunity.net/extension/blog-module-pro" target="_blank">Shopunity.net</a> <br> Find more amazing extensions at <a href="https://dreamvention.ee/" target="_blank">Dreamvention.ee</a>';
-
-?>
+$_['text_powered_by'] = 'Tested with <a href="https://shopunity.net/extension/blog-module-pro" target="_blank">Shopunity.net</a> <br> Find more amazing extensions at <a href="https://dreamvention.ee/" target="_blank">Dreamvention.ee</a>';
