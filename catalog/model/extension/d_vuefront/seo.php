052bd417e9ca0db724c4a4e329f91c40b11becc9
alexandrdrvn <php4.drvn@gmail.com>
2020-08-18 17:24:30 +0300

    fix: fix search on 2x

--- catalog/model/extension/d_vuefront/seo.php
+++ catalog/model/extension/d_vuefront/seo.php
@@ -2,48 +2,84 @@
 
 class ModelExtensionDVuefrontSeo extends Model
 {
-    public function searchKeyword($keyword) {
+    public function searchKeyword($keyword)
+    {
         $type = '';
         $id = 0;
-        if(VERSION >= '3.0.0.0') {
-			$parts = explode('/',  preg_replace("/^\//", "", $keyword));
+        if (VERSION >= '3.0.0.0') {
+            $parts = explode('/', preg_replace("/^\//", "", $keyword));
 
-			// remove any empty arrays from trailing
-			if (utf8_strlen(end($parts)) == 0) {
-				array_pop($parts);
+            // remove any empty arrays from trailing
+            if (utf8_strlen(end($parts)) == 0) {
+                array_pop($parts);
             }
 
             foreach ($parts as $part) {
                 $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "seo_url WHERE keyword = '" . $this->db->escape($part) . "' AND store_id = '" . (int)$this->config->get('config_store_id') . "'");
 
                 if ($query->num_rows) {
-					$url = explode('=', $query->row['query']);
+                    $url = explode('=', $query->row['query']);
 
-					if ($url[0] == 'product_id') {
+                    if ($url[0] == 'product_id') {
                         $type = 'product';
-						$id = $url[1];
-					}
+                        $id = $url[1];
+                    }
 
-					if ($url[0] == 'category_id') {
+                    if ($url[0] == 'category_id') {
                         $type = 'category';
                         $id = $url[1];
-					}
+                    }
 
-					if ($url[0] == 'manufacturer_id') {
+                    if ($url[0] == 'manufacturer_id') {
                         $type = 'manufacturer';
                         $id = $url[1];
-					}
+                    }
 
-					if ($url[0] == 'information_id') {
+                    if ($url[0] == 'information_id') {
                         $type = 'page';
                         $id = $url[1];
-					}
-				} else {
-					break;
-				}
+                    }
+                } else {
+                    break;
+                }
             }
         } else {
-            // $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "url_alias WHERE `query` LIKE 'product_id=".(int)$product_id."'");
+          $parts = explode('/', preg_replace("/^\//", "", $keyword));
+
+          // remove any empty arrays from trailing
+          if (utf8_strlen(end($parts)) == 0) {
+              array_pop($parts);
+          }
+
+          foreach ($parts as $part) {
+              $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "url_alias WHERE keyword = '" . $this->db->escape($part) . "'");
+
+              if ($query->num_rows) {
+                  $url = explode('=', $query->row['query']);
+
+                  if ($url[0] == 'product_id') {
+                      $type = 'product';
+                      $id = $url[1];
+                  }
+
+                  if ($url[0] == 'category_id') {
+                      $type = 'category';
+                      $id = $url[1];
+                  }
+
+                  if ($url[0] == 'manufacturer_id') {
+                      $type = 'manufacturer';
+                      $id = $url[1];
+                  }
+
+                  if ($url[0] == 'information_id') {
+                      $type = 'page';
+                      $id = $url[1];
+                  }
+              } else {
+                  break;
+              }
+          }
         }
 
         return array(
